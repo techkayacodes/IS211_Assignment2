465f7d36e8ab1b6f20f52a567b4609f11ca8fd04
Tech Kaya <techkayacodes@gmail.com>
2023-09-04 19:47:21 -0400

    editing script as return brings back error

--- IS211_Assignment2.py
+++ IS211_Assignment2.py
@@ -1,70 +1,66 @@
-import argparse
-import urllib.request
-import logging
-import datetime
+import argparse  # Import the argparse module to parse command line arguments
+import urllib.request  # Import urllib.request module to download data from a URL
+import logging  # Import the logging module for error logging
+import datetime  # Import the datetime module to work with dates and times
 
+# Function to download data from a given URL
 def downloadData(url):
     try:
-        # Try to open the URL and read its contents
         with urllib.request.urlopen(url) as response:
             data = response.read().decode('utf-8')
-        return data
+        return data  # Return the downloaded data
     except Exception as e:
-        # If there's an error while downloading, print an error message and exit
         print(f"Error downloading data: {str(e)}")
         exit(1)
 
+# Function to process the CSV data and create a dictionary mapping IDs to (name, birthday) tuples
 def processData(file_content):
-    personData = {}  # Create an empty dictionary to store person data
+    personData = {}  # Create an empty dictionary to store the processed data
     lines = file_content.split('\n')  # Split the file content into lines
     for line_num, line in enumerate(lines[1:], start=2):
-        # Loop through each line in the file, skipping the header line (index 0)
         parts = line.split(',')
         if len(parts) == 3:
-            # Check if there are three parts (ID, name, birthday) in the line
             user_id, name, birthday_str = parts
             try:
-                # Convert the birthday string to a datetime object
-                birthday = datetime.datetime.strptime(birthday_str, '%d/%m/%Y')
-                # Store the person's data in the dictionary with ID as the key
-                personData[int(user_id)] = (name, birthday)
+                birthday = datetime.datetime.strptime(birthday_str, '%d/%m/%Y').date()
+                personData[int(user_id)] = (name, birthday)  # Store data in the dictionary
             except ValueError:
-                # If there's an error parsing the date, log it with the line number and ID
+                # Log errors for malformed dates
                 logger.error(f"Error processing line #{line_num} for ID #{user_id}")
-    return personData
+    return personData  # Return the processed data
 
+# Function to display a person's information based on their ID
 def displayPerson(id, personData):
     if id in personData:
-        # If the ID exists in the person data, print the person's information
         name, birthday = personData[id]
+        # Print the person's information in the specified format
         print(f"Person #{id} is {name} with a birthday of {birthday.strftime('%Y %m %d')}")
     else:
-        # If the ID does not exist, print a message
-        print("No user found with that id")
+        print("No user found with that id")  # Print a message if the ID is not found
 
+# Main function that orchestrates the program
 def main(url):
-    csvData = downloadData(url)
+    csvData = downloadData(url)  # Download CSV data from the specified URL
     
-    # Configure logging to write errors to a file called 'errors.log'
+    # Configure logging to write errors to "errors.log" in the specified format
     logging.basicConfig(filename='errors.log', level=logging.ERROR, format='Error processing line #%(lineno)d for ID #%(id)s')
     logger = logging.getLogger('assignment2')
     
-    personData = processData(csvData)  # Process the CSV data into a dictionary
+    personData = processData(csvData)  # Process CSV data into a dictionary
     
     while True:
         user_input = input("Enter an ID to lookup (enter a negative number or 0 to exit): ")
         try:
             id = int(user_input)
             if id <= 0:
-                # If the user enters a negative number or 0, exit the program
-                break
+                break  # Exit the program if the user enters a non-positive number
             displayPerson(id, personData)  # Display the person's information
         except ValueError:
-            # If the user enters invalid input, prompt them to enter a valid ID
-            print("Invalid input. Please enter a valid ID.")
+            print("Invalid input. Please enter a valid ID.")  # Prompt for a valid ID if input is not an integer
 
+# Entry point of the script
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
     args = parser.parse_args()
-    main(args.url)
+    main(args.url)  # Call the main function with the provided URL as an argument
