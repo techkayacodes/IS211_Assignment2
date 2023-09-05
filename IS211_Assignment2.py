4fddafaddb1156ad2e71f6f48c7755c631be6824
Tech Kaya <techkayacodes@gmail.com>
2023-09-04 20:00:24 -0400

    Script is now working-asks for ID and sends info.

--- IS211_Assignment2.py
+++ IS211_Assignment2.py
@@ -1,66 +1,59 @@
-import argparse  # Import the argparse module to parse command line arguments
-import urllib.request  # Import urllib.request module to download data from a URL
-import logging  # Import the logging module for error logging
-import datetime  # Import the datetime module to work with dates and times
+import argparse
+import urllib.request
+import logging
+import datetime
 
-# Function to download data from a given URL
 def downloadData(url):
     try:
         with urllib.request.urlopen(url) as response:
             data = response.read().decode('utf-8')
-        return data  # Return the downloaded data
+        return data
     except Exception as e:
         print(f"Error downloading data: {str(e)}")
         exit(1)
 
-# Function to process the CSV data and create a dictionary mapping IDs to (name, birthday) tuples
-def processData(file_content):
-    personData = {}  # Create an empty dictionary to store the processed data
-    lines = file_content.split('\n')  # Split the file content into lines
+def processData(file_content, logger):  # Pass logger as an argument
+    personData = {}
+    lines = file_content.split('\n')
     for line_num, line in enumerate(lines[1:], start=2):
         parts = line.split(',')
         if len(parts) == 3:
             user_id, name, birthday_str = parts
             try:
-                birthday = datetime.datetime.strptime(birthday_str, '%d/%m/%Y').date()
-                personData[int(user_id)] = (name, birthday)  # Store data in the dictionary
+                birthday = datetime.datetime.strptime(birthday_str, '%d/%m/%Y')
+                personData[int(user_id)] = (name, birthday)
             except ValueError:
-                # Log errors for malformed dates
                 logger.error(f"Error processing line #{line_num} for ID #{user_id}")
-    return personData  # Return the processed data
+    return personData
 
-# Function to display a person's information based on their ID
 def displayPerson(id, personData):
     if id in personData:
         name, birthday = personData[id]
-        # Print the person's information in the specified format
         print(f"Person #{id} is {name} with a birthday of {birthday.strftime('%Y %m %d')}")
     else:
-        print("No user found with that id")  # Print a message if the ID is not found
+        print("No user found with that id")
 
-# Main function that orchestrates the program
 def main(url):
-    csvData = downloadData(url)  # Download CSV data from the specified URL
+    csvData = downloadData(url)
     
-    # Configure logging to write errors to "errors.log" in the specified format
+    # Configure logging to write errors to a file called 'errors.log'
     logging.basicConfig(filename='errors.log', level=logging.ERROR, format='Error processing line #%(lineno)d for ID #%(id)s')
     logger = logging.getLogger('assignment2')
     
-    personData = processData(csvData)  # Process CSV data into a dictionary
+    personData = processData(csvData, logger)  # Pass logger to processData
     
     while True:
         user_input = input("Enter an ID to lookup (enter a negative number or 0 to exit): ")
         try:
             id = int(user_input)
             if id <= 0:
-                break  # Exit the program if the user enters a non-positive number
-            displayPerson(id, personData)  # Display the person's information
+                break
+            displayPerson(id, personData)
         except ValueError:
-            print("Invalid input. Please enter a valid ID.")  # Prompt for a valid ID if input is not an integer
+            print("Invalid input. Please enter a valid ID.")
 
-# Entry point of the script
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
     args = parser.parse_args()
-    main(args.url)  # Call the main function with the provided URL as an argument
+    main(args.url)
